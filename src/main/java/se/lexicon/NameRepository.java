609423d403925374b60b1a0d6733b5248f063b54
Madhu <madhu8129@gmail.com>
2023-11-29 11:27:15 +0100

    Step2 completed

--- src/main/java/se/lexicon/NameRepository.java
+++ src/main/java/se/lexicon/NameRepository.java
@@ -14,6 +14,20 @@ public class NameRepository {
         nameArray=findAll(names);
         System.out.println("Copied Array:");
         System.out.println(Arrays.toString(nameArray));
+        String stringFound=find("Anna");
+        System.out.println(stringFound);
+
+        boolean value=add("abcd");
+        value=add("abcd");
+        value=add("xyz");
+        if (value) {
+            System.out.println("A new name added ");
+            System.out.println("Array after added a name:");
+            System.out.println(Arrays.toString(names));
+        }
+        else {
+            System.out.println("Name already exists!!!");
+        }
     }
 
     public static int getSize(String[] names) {
@@ -38,5 +52,26 @@ public class NameRepository {
        String[] newArray= Arrays.copyOf(strArray,strArray.length);
        return newArray;
 
+    }
+    public static String find(final String fullName) {
+        for (String name : names) {
+            if (name.equals(fullName)) {
+                return name;
+            }
+        }
+        return null;
+    }
+    public static boolean add(final String fullName){
+        for(String name:names){
+            if(name==fullName){
+                return false;
+            }
+        }
+        String[] newArray=Arrays.copyOf(names,names.length+1);
+        newArray[newArray.length-1]=fullName;
+        clear();
+        names=Arrays.copyOf(newArray,newArray.length);
+       //System.out.println(Arrays.toString(newArray));
+        return true;
     }
 }
