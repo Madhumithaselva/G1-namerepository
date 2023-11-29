03e7a583222e2eb4b103e13339911dbfce8f7150
Christian Myrén <85617763+PLUPPERT@users.noreply.github.com>
2023-11-29 14:15:27 +0100

    - Working with update method

--- src/main/java/se/lexicon/NameRepository.java
+++ src/main/java/se/lexicon/NameRepository.java
@@ -1,5 +1,6 @@
 package se.lexicon;
 
+import java.sql.Array;
 import java.util.Arrays;
 
 public class NameRepository {
@@ -10,9 +11,10 @@ public class NameRepository {
         setNames(nameArray);
 
         add("Anna Panna");
-        String[] namesFound = findByFirstName("Kalle");
-        System.out.println(Arrays.toString(namesFound));
 
+        String[] findLastNameArray = findByLastName("Panna");
+
+        boolean updateCheck = update("Anna Panna", "Gerd Abc" );
     }
 
     public static int getSize(String[] names) {
@@ -31,8 +33,7 @@ public class NameRepository {
     }
 
     public static String[] findAll(String[] strArray) {
-        String[] newArray= Arrays.copyOf(strArray,strArray.length);
-        return newArray;
+        return Arrays.copyOf(strArray,strArray.length);
 
     }
     public static String find(final String fullName) {
@@ -67,11 +68,65 @@ public class NameRepository {
 
         for (int i = 0; i < names.length; i++) {
             tmp = names[i].replaceAll("\\s.*", "");
-            if (firstName.equals(names[i])) {
+            if (firstName.equals(tmp)) {
                 arrayLength++;
             }
-
         }
+        if (arrayLength > 0) {
+            newNamesArray = new String[arrayLength];
+            int index = 0;
+
+            for (String name : names) {
+                if (name.toLowerCase().contains(firstName.toLowerCase())) {
+                    newNamesArray[index] = name;
+                    index++;
+                }
+            }
+            System.out.println("Returning new array of: ");
+            System.out.println(Arrays.toString(newNamesArray));
+            return newNamesArray;
+        }
+        System.out.println("No name found matching: " + firstName);
+
         return null;
+    }
+
+    public static String[] findByLastName(final String lastName) {
+        String[] newNamesArray;
+        int arrayLength = 0;
+        String tmp;
+
+        for (int i = 0; i < names.length; i++) {
+            tmp = names[i].substring(names[i].indexOf(" ") + 1).trim();
+            if (lastName.equals(tmp)) {
+                arrayLength++;
+            }
+        }
+        if (arrayLength > 0) {
+            newNamesArray = new String[arrayLength];
+            int index = 0;
+
+            for (String name : names) {
+                if (name.toLowerCase().contains(lastName.toLowerCase())) {
+                    newNamesArray[index] = name;
+                    index++;
+                }
+            }
+            System.out.println("Returning new array of: ");
+            System.out.println(Arrays.toString(newNamesArray));
+            return newNamesArray;
+        }
+        System.out.println("No name found matching: " + lastName);
+
+        return null;
+    }
+
+    public static boolean update(final String original, final String updatedName) {
+        for (int i = 0; i < names.length; i++) {
+            if (names[i].toLowerCase().equals(original.toLowerCase())) {
+                names[i] = updatedName;
+                return true;
+            }
+        }
     }
 }
